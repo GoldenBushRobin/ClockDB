f6a86eab1ce9ee20d7dcb6b246b59bc989a60443
goldenbushrobin <goldenbushrobin1@gmail.com>
2026-04-03 05:05:01 -0500

    moved constants to own file

---
+++ include/constants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#define MAX_CLOCK 410338673
+
+#define BLOCK_COUNT 256ULL // must be a power of 2
+#define BLOCK_SIZE 0x100000000ULL / BLOCK_COUNT
+
+#define SEG_COUNT 17
+#define SEG_SIZE (MAX_CLOCK / SEG_COUNT)
+
+inline int CLOCKOFFS[7] = {24137569, 1419857, 83521, 4913, 289, 17, 1};
--- include/utils.h
+++ include/utils.h
@@ -7,6 +7,7 @@ extern "C" {
 void sfmt_skip_rands(sfmt_t * sfmt, uint32_t skipped);
 
 #include "parallel_radix_sort.h"
+#include "constants.h"
 
 #include <cstdint>
 #include <vector>
@@ -17,13 +18,6 @@ void sfmt_skip_rands(sfmt_t * sfmt, uint32_t skipped);
 #define psort32 parallel_radix_sort::PairSort<uint32_t,uint32_t>
 
 #define LOG_INFO(fmt, ...) fprintf(stderr, "[%s] " fmt "\n", std::format("{:%F %T}", std::chrono::system_clock::now()).c_str(), ##__VA_ARGS__)
-
-#define BLOCK_COUNT 256ULL // must be a power of 2
-#define BLOCK_SIZE 0x100000000ULL / BLOCK_COUNT
-
-#define SEG_COUNT 17
-#define MAX_CLOCK 410338673
-#define SEG_SIZE (MAX_CLOCK / SEG_COUNT)
 
 void writeVector(std::vector<uint32_t>& vec, size_t size, std::string fname);
 void readVector(std::vector<uint32_t>& vec, size_t size, std::string fname);
